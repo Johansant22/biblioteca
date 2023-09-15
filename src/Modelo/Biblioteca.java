2a5de8ecf9fd902c6a1e1ed8180a17352ca491f1
Johansant22 <johansan799@gmail.com>
2023-09-14 23:47:56 -0500

    biblioteca

---
+++ src/Modelo/Biblioteca.java
@@ -0,0 +1,43 @@
+package Modelo;
+
+import java.util.ArrayList;
+
+public class Biblioteca {
+private String nombre;
+private ArrayList<Libro> coleccionLibros = new ArrayList<>();
+private ArrayList<Revista> coleccionRevistas = new ArrayList<>();
+
+public Biblioteca(String nombre) {
+this.nombre = nombre;
+}
+
+public void anadirLibro(Libro libro) {
+coleccionLibros.add(libro);
+}
+
+public String getNombre() {
+    return nombre;
+}
+
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+
+public void anadirRevista(Revista revista) {
+coleccionRevistas.add(revista);
+}
+
+public void listarLibros() {
+System.out.println("Libros en la biblioteca:");
+for (Libro libro : coleccionLibros) {
+libro.imprimirDetalles();
+}
+}
+
+public void listarRevistas() {
+System.out.println("Revistas en la biblioteca:");
+for (Revista revista : coleccionRevistas) {
+revista.imprimirDetalles();
+}
+}
+}
